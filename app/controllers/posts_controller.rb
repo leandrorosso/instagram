3109c59a102b15bea8e4da9c4c1ae57ab21d6807
Leandro Rosso <lrosso.ti@gmail.com>
2022-07-08 18:24:02 -0300

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,40 @@
+class PostsController < ApplicationController
+  include SuggestedUsers
+
+  before_action :set_post, only: %i[ show ]
+  before_action :set_suggested_users, only: %i[index]
+  
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params.merge(created_by: current_user))
+
+    if @post.save
+      redirect_to post_url(@post), notice: "Post foi criado com sucesso."
+    else
+      flash.now[:alert] = @post.errors.full_messages.to_sentence 
+      render :new
+    end
+
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:photo, :description)
+  end
+
+  end
